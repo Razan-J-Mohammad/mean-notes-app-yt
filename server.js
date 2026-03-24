b46325a910d5b58d71905c7b01993c3ea97b85dc
Razan Nazzal <razanmohmj20@gmail.com>
2026-03-24 17:42:11 +0200

    added morgan logger

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 import express from 'express';
 import dotenv from  "dotenv";
+import morgan from 'morgan';
+
 dotenv.config({
     path : "./config/config.env",
 });
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res)=>{
+    res.send("Hello World ");
+});
+app.listen(3000,()=>{
+console.log("server is up and runing 30000!")
+});
